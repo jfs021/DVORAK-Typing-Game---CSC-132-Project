cdde72198fbd0a3d6d8e84f0a3061e4748d8ec38
Maygoalb <165967934+Maygoalb@users.noreply.github.com>
2024-04-08 22:13:24 -0500

    Basic GUI features was added
    page1 , page 2, RusultScreen class

--- Maygoal.py
+++ Maygoal.py
@@ -1,3 +1,34 @@
-def CheckInput(x):
-    if x == 1:
-        pass
+from tkinter import *
+class ResultScreen(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.master = master
+        self.load_pages()
+    def load_pages(self):
+        self.page1 = Frame(self.master)
+        self.page1.grid(row = 0, column = 0, sticky = N + W + E + S )
+        self.page2 = Frame(self.master)
+        self.page2.grid(row = 0, column = 0, sticky = N + W + E + S )
+        # setting up page 1
+        self.b1 = Button(self.page1, text = "START", command = lambda:self.SetUpFirstPage())
+        # setting up for page 2
+        self.b2 = Button(self.page2, text = "Restart", command = lambda:self.page1.tkraise())
+        self.l2 = Label(self.page2, text = "WPM")
+
+
+#main
+win1=Tk()
+win1.geometry("500x500")
+win1.title("Speed Typers")
+app = App(win1)
+win1.mainloop()                
+
+def CheckInput(listOfWords, listOfInput):
+    for i in len(listOfWords):
+        if listOfInput == listOfWords[i]:
+            pass
+        else:
+            pass
+            
+    
+   
